4eed50f2a875ad4ee0f9bc6f22b9a5f2fe85e2cc
Stefan Kühn <ducdebreme@early-dance.de>
2014-01-04 22:22:34 +0100

    - added live-reload - added header via include - added basic styling - created a demo page for page layout

--- config.rb
+++ config.rb
@@ -65,7 +65,7 @@ end
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -83,6 +83,8 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
+
+set :partials_dir, 'partials'
 
 activate :livereload
 
